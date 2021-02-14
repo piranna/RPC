a7753260bcc5b7624d09661575fd26e1624253e0
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-02-14 17:22:25 +0100

    [JsonRpc] Check for `null` as ID on requests and response of malformed

--- __tests__/JsonRpc.js
+++ __tests__/JsonRpc.js
@@ -28,60 +28,88 @@ test("basic", function () {
       const result = jsonRpc.onMessage(response);
 
       return expect(result).resolves.toBeUndefined();
-    })
-  ]),
-  expect(request).resolves.toBe("bar 2");
+    }),
+    expect(request).resolves.toBe("bar 2")
+  ])
 });
 
-test("Invalid JsonRPC version 'undefined'", function () {
-  const jsonRpc = new JsonRpc();
+describe("onMessage", function () {
+  test("Invalid JsonRPC version 'undefined'", function () {
+    const jsonRpc = new JsonRpc();
 
-  const result = jsonRpc.onMessage('{"id": 0}')
+    const result = jsonRpc.onMessage('{"id": 0}');
 
-  return expect(result).resolves.toEqual('{"error":{"code":-32600,"message":"Invalid JsonRPC version \'undefined\'"},"id":0,"jsonrpc":"2.0"}');
-});
+    return expect(result).resolves.toEqual(
+      '{"error":{"code":-32600,"message":"Invalid JsonRPC version \'undefined\'"},"id":0,"jsonrpc":"2.0"}'
+    );
+  });
 
-test("Invalid JSON", function () {
-  const jsonRpc = new JsonRpc();
+  test("Invalid JSON", function () {
+    const jsonRpc = new JsonRpc();
 
-  const result = jsonRpc.onMessage('foo')
+    const result = jsonRpc.onMessage("foo");
 
-  return expect(result).resolves.toEqual('{"error":{"code":-32700,"data":{},"message":"Invalid JSON"},"id":null,"jsonrpc":"2.0"}')
-});
+    return expect(result).resolves.toEqual(
+      '{"error":{"code":-32700,"data":{},"message":"Invalid JSON"},"id":null,"jsonrpc":"2.0"}'
+    );
+  });
 
-test("No methods", function () {
-  const jsonRpc = new JsonRpc();
+  test("No methods", function () {
+    const jsonRpc = new JsonRpc();
 
-  const result = jsonRpc.onMessage('{"jsonrpc":"2.0","method":"foo"}');
+    const result = jsonRpc.onMessage('{"jsonrpc":"2.0","method":"foo"}');
 
-  return expect(result).rejects.toMatchInlineSnapshot(`
-    Object {
-      "code": -32603,
-      "message": "Client doesn't accept requests",
-    }
-  `);
-});
+    return expect(result).rejects.toMatchInlineSnapshot(`
+      Object {
+        "code": -32603,
+        "message": "Client doesn't accept requests",
+      }
+    `);
+  });
 
-test("Method not found", function () {
-  const jsonRpc = new JsonRpc({});
+  test("Method not found", function () {
+    const jsonRpc = new JsonRpc({});
 
-  const result = jsonRpc.onMessage('{"jsonrpc":"2.0","method":"foo"}');
+    const result = jsonRpc.onMessage('{"jsonrpc":"2.0","method":"foo"}');
 
-  return expect(result).rejects.toMatchInlineSnapshot(`
-    Object {
-      "code": -32601,
-      "data": "foo",
-      "message": "Unknown method 'foo'",
-    }
-  `);
+    return expect(result).rejects.toMatchInlineSnapshot(`
+      Object {
+        "code": -32601,
+        "data": "foo",
+        "message": "Unknown method 'foo'",
+      }
+    `);
+  });
+
+  test("Response of failed notification", function () {
+    const jsonRpc = new JsonRpc();
+
+    const result = jsonRpc.onMessage('{"jsonrpc":"2.0","id":null}');
+
+    return expect(result).rejects.toMatchInlineSnapshot(`undefined`);
+  });
+
+  test("Request with `null` id", function () {
+    const jsonRpc = new JsonRpc({});
+
+    const result = jsonRpc.onMessage(
+      '{"jsonrpc":"2.0","id":null,"method":"foo"}'
+    );
+
+    return expect(result).resolves.toEqual(
+      '{"error":{"code":-32601,"data":"foo","message":"Unknown method \'foo\'"},"id":null,"jsonrpc":"2.0"}'
+    );
+  });
 });
 
 test("notification", function () {
   const jsonRpc = new JsonRpc();
 
-  const notification = jsonRpc.notification('foo', ['bar'])
+  const notification = jsonRpc.notification("foo", ["bar"]);
 
-  expect(notification).toEqual('{"jsonrpc":"2.0","method":"foo","params":["bar"]}')
+  expect(notification).toEqual(
+    '{"jsonrpc":"2.0","method":"foo","params":["bar"]}'
+  );
 });
 
 test("Failed notification", function () {
