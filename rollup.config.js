8d82402c7bf4d75c9693054b0afa7fa8e9a4e293
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-09-25 09:42:06 +0200

    Remove usage of Rollup for ES modules & fix-up generation of CommonJS

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,21 @@ import fs from 'fs';
 
 const LIB_DIR = path.join('.', 'lib');
 const CJS_DIR = path.join('.', 'dist', 'cjs');
-const ES_DIR = path.join('.', 'dist', 'es');
 
 const files = fs.readdirSync(LIB_DIR);
 console.log(files);
 
 const config = files.map((file) => ({
+    external: [
+        '.', '@journeyapps/domparser', 'crypto', 'error-to-json', 'object-path',
+        'xmlrpc-serialization'
+    ],
     input: path.join(LIB_DIR, file),
     output: [
         {
+            exports: 'default',
             file: path.join(CJS_DIR, file),
             format: 'cjs',
-            sourcemap: false,
-        }, {
-            file: path.join(ES_DIR, file),
-            format: 'es',
             sourcemap: false,
         }
     ]
