d453dff4be0205cf65810a8630b5150e82036351
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-09-27 09:24:35 +0200

    Set `.cjs` extension to generated CommonJS files

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,30 @@
-import path from 'path';
 import fs from 'fs';
+import { basename, join } from 'path';
 
-const LIB_DIR = path.join('.', 'lib');
-const CJS_DIR = path.join('.');
+const LIB_DIR = join('.', 'lib');
+const CJS_DIR = join('.');
 
 const files = fs.readdirSync(LIB_DIR);
 
-const config = files.map((file) => ({
-    external: [
-        '.', '@journeyapps/domparser', 'crypto', 'error-to-json', 'object-path',
-        'xmlrpc-serialization'
-    ],
-    input: path.join(LIB_DIR, file),
-    output: [
-        {
-            exports: 'default',
-            file: path.join(CJS_DIR, file),
-            format: 'cjs',
-            sourcemap: false,
-        }
-    ]
-}));
+const config = files.map(function(file)
+{
+    const outputFile = basename(file, '.js') + '.cjs';
+
+    return {
+        external: [
+            '.', '@journeyapps/domparser', 'crypto', 'error-to-json',
+            'object-path', 'xmlrpc-serialization'
+        ],
+        input: join(LIB_DIR, file),
+        output: [
+            {
+                exports: 'default',
+                file: join(CJS_DIR, outputFile),
+                format: 'cjs',
+                sourcemap: false,
+            }
+        ]
+    }
+});
 
 export default config;
