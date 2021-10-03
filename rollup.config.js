fa7c38b82ec6e51e2d02be07433f2bc0a5282c8f
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2021-10-03 10:36:40 +0200

    Fix imports & update `package-lock.json`

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,11 @@ import { basename, join } from 'path';
 const LIB_DIR = join('.', 'lib');
 const CJS_DIR = join('.');
 
+const external = [
+    '.', '@journeyapps/domparser', '@xmldom/xmldom', 'crypto', 'error-to-json',
+    'object-path', 'xmlrpc-serialization'
+]
+
 const files = fs.readdirSync(LIB_DIR);
 
 const config = files.map(function(file)
@@ -11,10 +16,7 @@ const config = files.map(function(file)
     const outputFile = basename(file, '.js') + '.cjs';
 
     return {
-        external: [
-            '.', '@journeyapps/domparser', 'crypto', 'error-to-json',
-            'object-path', 'xmlrpc-serialization'
-        ],
+        external,
         input: join(LIB_DIR, file),
         output: [
             {
